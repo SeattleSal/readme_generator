6108e773b42e2771483c490f1598a8e4662daec0
Sally Perez <seattlesally1@gmail.com>
2020-11-25 22:25:02 -0800

    installed inquirer, added question prompts as object

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 // array of questions for user
 const questions = [
-
+        {
+          type: 'input',
+          message: 'What is your GitHub username?',
+          name: 'username',
+        },
+        {
+          type: 'input',
+          message: 'What is your email address?',
+          name: 'email',
+        },
+        {
+          type: 'input',
+          message: `What is your project's name?`,
+          name: 'pname',
+        },
+        {
+        type: 'input',
+        message: 'Please write a short description of your project.',
+        name: 'pdescription',
+        },
+        {
+        type: 'input', // is this a checkbox orother type?
+        message: 'What kind of license should your project have?',
+        name: 'license',
+        },
+        {
+        type: 'input',
+        message: `What command should be run to install dependencies?`,
+        name: 'dependencies',
+        },
+        {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'tests',
+        },
+        {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'repo',
+        },
+        {
+        type: 'input',
+        message: `What does the user need to know about contributing to the repo?`,
+        name: 'contribute',
+        }
 ];
 
 // function to write README file
@@ -9,7 +57,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    console.log(`gonna be fun!`);
 }
 
 // function call to initialize program
