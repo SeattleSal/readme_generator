8e543d88b128bf779dd8aa95f1145e809311fdaa
Sally Perez <seattlesally1@gmail.com>
2020-11-30 21:28:24 -0800

    continued building out readme text string, write to readme.md

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# readme_generator
+README for seattle sal
+  ## readme stuff
+
+  ## Table of Contents
+
+  ## Description
+
+  dlajfl;fjdkj
+  ## Links
+  ## Dependencies
+  The following commands should be run for dependencies: npm bla bla
+  ## Use
+  useful info
+  ## Tests
+  The following commands should be run for testing: testing 123
+  ## How to Contribute
+  contribute good
+  ## Contact
+  sal@g
+  ## License
+  dlajfl;fjdkj    
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const fileName = "README-1.md"
-
+const fileName = "README.md"
 
 // array of questions for user
 const questions = [
@@ -29,39 +28,59 @@ const questions = [
         type: 'input', // is this a checkbox orother type?
         message: 'What kind of license should your project have?',
         name: 'license',
+        },
+        {
+        type: 'input',
+        message: `What command should be run to install dependencies?`,
+        name: 'dependencies',
+        },
+        {
+        type: 'input',
+        message: 'What command should be run to run tests?',
+        name: 'tests',
+        },
+        {
+        type: 'input',
+        message: 'What does the user need to know about using the repo?',
+        name: 'repo',
+        },
+        {
+        type: 'input',
+        message: `What does the user need to know about contributing to the repo?`,
+        name: 'contribute',
         }
-        // ,
-        // {
-        // type: 'input',
-        // message: `What command should be run to install dependencies?`,
-        // name: 'dependencies',
-        // },
-        // {
-        // type: 'input',
-        // message: 'What command should be run to run tests?',
-        // name: 'tests',
-        // },
-        // {
-        // type: 'input',
-        // message: 'What does the user need to know about using the repo?',
-        // name: 'repo',
-        // },
-        // {
-        // type: 'input',
-        // message: `What does the user need to know about contributing to the repo?`,
-        // name: 'contribute',
-        // }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    console.log(fileName);
-    console.log(data);
+    // console.log(fileName);
+    // console.log(data);
+    let rmText = 
+  `README for ${data.username}
+  ## ${data.pname}
+  ## Table of Contents
+  ## Description
+  ${data.pdescription}
+  ## Links
+  ## Dependencies
+  The following commands should be run for dependencies: ${data.dependencies}
+  ## Use
+  ${data.repo}
+  ## Tests
+  The following commands should be run for testing: ${data.tests}
+  ## How to Contribute
+  ${data.contribute}
+  ## Contact
+  ${data.email}
+  ## License
+  ${data.pdescription}`;
+
+    fs.writeFile(fileName, rmText, (err) =>
+    err ? console.log(err) : console.log('Success! README created!'));
 }
 
 // function to initialize program
 function init() {
-    console.log(`gonna be fun!`);
     inquirer.prompt(questions)
     .then((answers) => writeToFile(fileName, answers))
     .catch(error => {
