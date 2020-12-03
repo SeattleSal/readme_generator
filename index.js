092720fb405c0ca84deb709e43e50767f0f5e7b2
Sally Perez <seattlesally1@gmail.com>
2020-12-03 12:06:16 -0800

    added licencse choices and license text per user selection, added license badge

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
-const fileName = "README_OUTPUT.md"
+const gm = require('./utils/generateMarkdown');
+const fileName = "README_OUTPUT.md";
+const licenseChoices = [{name:"MIT", value:0}, 
+                        {name:"GNU General Public License v3.0", value: 1}, 
+                        {name:"Unlicense", value:2},
+                        {name:"none", value:3}];
 
 // array of questions for user
 const questions = [
@@ -21,34 +25,35 @@ const questions = [
           name: 'pname',
         },
         {
-        type: 'input',
-        message: 'Please write a short description of your project.',
-        name: 'pdescription',
+          type: 'input',
+          message: 'Please write a short description of your project.',
+          name: 'pdescription',
         },
         {
-        type: 'input', // is this a checkbox orother type?
-        message: 'What kind of license should your project have?',
-        name: 'license',
+          type: 'list', 
+          message: 'What kind of license should your project have?',
+          name: 'license',
+          choices: licenseChoices
         },
         {
-        type: 'input',
-        message: `What command should be run to install dependencies?`,
-        name: 'dependencies',
+          type: 'input',
+          message: `What command should be run to install dependencies?`,
+          name: 'dependencies',
         },
         {
-        type: 'input',
-        message: 'What command should be run to run tests?',
-        name: 'tests',
+          type: 'input',
+          message: 'What command should be run to run tests?',
+          name: 'tests',
         },
         {
-        type: 'input',
-        message: 'What does the user need to know about using the repo?',
-        name: 'repo',
+          type: 'input',
+          message: 'What does the user need to know about using the repo?',
+          name: 'repo',
         },
         {
-        type: 'input',
-        message: `What does the user need to know about contributing to the repo?`,
-        name: 'contribute',
+          type: 'input',
+          message: `What does the user need to know about contributing to the repo?`,
+          name: 'contribute',
         }
 ];
 
@@ -56,7 +61,7 @@ const questions = [
 function writeToFile(fileName, data) {
     // console.log(fileName);
     // console.log(generateMarkdown(data));
-    fs.writeFile(fileName, generateMarkdown(data), (err) =>
+    fs.writeFile(fileName, gm(data), (err) =>
     err ? console.log(err) : console.log('Success! README created!'));
 }
 
